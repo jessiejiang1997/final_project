4c38018d8d2db1968f06a7c4e3ab81cd1a6432aa
jessiejiang1997 <xiaoyu_jiang@brown.edu>
2020-04-30 01:41:42 -0400

    check dimension

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ style_paths = ['Vincent_van_Gogh_69.jpg', 'Vincent_van_Gogh_604.jpg']
 #image.py用的github上的原码，我过几天再更新一版
 
 if __name__ == "__main__":
-    best, best_loss = model.run(content_path, style_paths, iteration=1000)
+    best, best_loss = model.run(content_path, style_paths, iteration=200)
     image.saveimg(best, 'best.jpg')
